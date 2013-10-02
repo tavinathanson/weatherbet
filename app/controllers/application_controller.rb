32ac74a77bc0816f80e542e36815537d8137245c
tavi <tavi.nathanson@gmail.com>
2013-10-02 11:07:16 -0400

    Removing a colon after 'def index'.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def index:
+  def index
 		data = RestClient.get 'http://api.openweathermap.org/data/2.1/find/name?q=nyc&units=imperial'
 		data = JSON.parse(data)	
 
 		render text: "The weather in NYC is " + data["list"][0]["main"]["temp"].to_s + "° F"
 	end
 end
+
