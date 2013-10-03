02ec93442a28ef10852da0e7abb2b21a923ae249
tavi <tavi.nathanson@gmail.com>
2013-10-03 15:58:59 -0400

    A first attempt at a basic JavaScript toggle. It doesn't work, yet.

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,12 @@
+# Regular JS
+
+# A simple function to toggle an element between showing and hiding
+function toggle(obj) {
+	var el = document.getElementById(obj);
+
+	if (el.style.display != 'none') {
+		el.style.display = 'none';
+	} else {
+		el.style.display = '';
+	}
+}
