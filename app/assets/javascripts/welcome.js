5518b47c40c66adcc6bfffb22692e3e65a0ca476
tavi <tavi.nathanson@gmail.com>
2013-10-07 14:43:02 -0400

    Removing some unnecessary JS, and changing JS to camelCase.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,20 +2,10 @@
 
 // A function to toggle an element between showing and hiding
 function toggle(obj) {
-	toggle_helper(obj, false);
-}
-
-// A function to hide an element
-function hide_obj(obj) {
-	toggle_helper(obj, true);
-}
-
-// Helper function for toggle(...) and hide_obj(...)
-function toggle_helper(obj, force_hide) {
 	// TODO: Better error handling
 	var el = document.getElementById(obj);
 
-	if (force_hide || el.className != 'hide') {
+	if (el.className != 'hide') {
 		el.className = 'hide';
 	} else {
 		el.className = '';
@@ -23,7 +13,7 @@ function toggle_helper(obj, force_hide) {
 }
 
 // A function to switch *between* two objects
-function toggle_between(obj1, obj2) {
+function toggleBetween(obj1, obj2) {
 	toggle(obj1);
 	toggle(obj2);
 }
