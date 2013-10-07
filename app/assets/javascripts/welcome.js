aa6d9df24d1fe5cb67f564cf6d5d5f1aa869409a
tavi <tavi.nathanson@gmail.com>
2013-10-07 11:41:07 -0400

    First attempt at a SF/NYC toggle. The body onload in index.html.haml and application.html.haml is totally broken, so this clearly is not a working solution yet.

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,12 +1,29 @@
 // Regular JS
 
-// A simple function to toggle an element between showing and hiding
+// A function to toggle an element between showing and hiding
 function toggle(obj) {
+	toggle_helper(obj, false);
+}
+
+// A function to hide an element
+function hide_obj(obj) {
+	toggle_helper(obj, true);
+}
+
+// Helper function for toggle(...) and hide_obj(...)
+function toggle_helper(obj, force_hide) {
+	// TODO: Better error handling
 	var el = document.getElementById(obj);
 
-	if (el.style.display != 'none') {
+	if (force_hide || is_shown(el)) {
 		el.style.display = 'none';
 	} else {
-		el.style.display = '';
+		e.style.display = '';
 	}
+}
+
+// A function to switch *between* two objects
+function toggle_between(obj1, obj2) {
+	toggle(obj1);
+	toggle(obj2);
 }
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,15 @@
 class WelcomeController < ApplicationController
   def index
-		data = RestClient.get 'http://api.openweathermap.org/data/2.1/find/name?q=nyc&units=imperial'
-		data = JSON.parse(data)
-		@text = "The weather in NYC is seriously " + data["list"][0]["main"]["temp"].to_s + "° F"
-
+		@nyc_text = get_weather_text("nyc,ny", "New York City")
+		@sf_text = get_weather_text("sf,ca", "San Francisco")
 		render "welcome/index"
+	end
+
+	def get_weather_text(city_name_query, city_name_print)
+		# TODO: Better error handling
+		data = RestClient.get "http://api.openweathermap.org/data/2.1/find/name?q=" + city_name_query + "&units=imperial"
+		data_parsed = JSON.parse(data)
+
+		return "The weather in " + city_name_print + " is seriously " + data_parsed["list"][0]["main"]["temp"].to_s + "° F"
 	end
 end
